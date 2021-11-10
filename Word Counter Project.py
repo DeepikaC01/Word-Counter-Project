cb665fa6c2fd32e299f8994e1f9517ca0e2d86f8
Deepika C <93979075+DeepikaC01@users.noreply.github.com>
2021-11-10 12:28:42 +0530

    Add files via upload

---
+++ Word Counter Project.py
@@ -0,0 +1,8 @@
+# Text_Variable = "Deepika is a good girl. Deepika is very smart."
+# Count = 0
+# Word_List = Text_Variable.split(" ")
+# print(Word_List)
+# for Every_Word in Word_List:
+#     if("Deepika" == Every_Word):
+#         Count = Count + 1
+# print(Count)
